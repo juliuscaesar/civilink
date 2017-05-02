1b57c663042c89a9b2b25a2af96b97afd725912d
Caesar Nuzzolo <nuzzolo.c@husky.neu.edu>
2017-05-01 20:00:46 -0400

    Cause Tag component

---
+++ client/src/community/CauseTag.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Card, Icon, Image, Label } from 'semantic-ui-react'
+
+/**
+* Component for the Issue tag
+*/
+class CauseTag extends React.Component {
+  constructor(props) {
+    super(props);
+
+    /**
+    * Fields in this form are kept as state and initialized as empty
+    */
+    this.state = {
+      education: {"color": "teal", "icon": "student"},
+      environment: {"color": "green", "icon": "tree"}
+    };
+
+    this.getIcon = this.getIcon.bind(this);
+  }
+
+  /**
+  * Gets the Icon associated with the given cause
+  */
+  getIcon() {
+    switch (this.props.cause) {
+      case "Education":
+        return this.state.education.icon;
+        break;
+      case "Environment":
+        return this.state.environment.icon;
+        break;
+    }
+  }
+
+  /**
+  * Gets the color associated with the given cause
+  */
+  getColor() {
+    switch (this.props.cause) {
+      case "Education":
+        return this.state.education.color;
+        break;
+      case "Environment":
+        return this.state.environment.color;
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <Label
+        as='a'
+        content={this.props.cause}
+        icon={this.getIcon()}
+        color={this.getColor()}
+        size='big'
+      />
+    );
+  }
+}
+
+export default CauseTag;
