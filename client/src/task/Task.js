a7401d0f6b4afb7363ec42a9bb238158487c4455
Caesar Nuzzolo <nuzzolo.c@husky.neu.edu>
2017-05-18 12:57:32 -0400

    Task component

---
+++ client/src/task/Task.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Card, Label } from 'semantic-ui-react'
+
+/**
+* Component for Tasks
+*/
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+
+    /**
+    * Fields in this form are kept as state and initialized as empty
+    */
+    this.state = {
+      tasks: [],
+      errorMessage: '',
+      displayError: false
+    };
+  }
+
+  // Render the static content
+  render(){
+    return (
+      <Card fluid>
+        <Card.Content>
+          <Card.Header>
+              {this.props.task.title}
+          </Card.Header>
+          <Card.Meta>
+            <span className='date'>
+
+            </span>
+          </Card.Meta>
+          <Card.Description>
+            {this.props.task.desc}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
+
+export default Task;
